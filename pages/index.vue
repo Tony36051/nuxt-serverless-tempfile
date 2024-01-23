ce7456aefe60b7d16ceee11bfcc32ee489dd3987
w00406273 <w00406273@huawei.com>
2024-01-23 15:46:31 +0800

    a

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+<h1>hello world</h1>
+<NuxtLink to="/ranks">ranks</NuxtLink>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/ranks.vue
@@ -0,0 +1,12 @@
+<template>
+<h1>ranks </h1>
+<NuxtLink to="/">index</NuxtLink>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
